8155e34b64ca6b36df4057e0ecfa7f182769c9d8
diamond285 <59549446+diamond285@users.noreply.github.com>
2024-03-03 11:55:13 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+@st.cache_data
+def load_data(value):
+
+    import google.generativeai as genai
+    API_KEY = 'AIzaSyDN8avHb3uwrEhIOtPj7PhkXlpUNeAdsh8'
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel('gemini-pro')
+    chat = model.start_chat(history=[])
+    r1 = chat.send_message(value)
+    return r1.text
+
+st.header("Рекомендации AI для набора массы")
+
+height = st.number_input("Рост (см)", value=170)
+weight = st.number_input("Вес (кг)", value=60)
+gender = st.selectbox("Пол", ['Мужчина', 'Женщина'])
+
+btn = st.button("Отправить")
+if btn:
+    query = f"Как можно похудеть или набрать массуы при весе {weight} кг и при росте {height} см {gender}"
+
+    r = load_data(query)
+    if r:
+        st.write(r)
