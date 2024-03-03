e71c54af366b6ecf61c1219fdbdf49bff6fd5677
diamond285 <59549446+diamond285@users.noreply.github.com>
2024-03-03 12:50:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import streamlit as st
 def load_data(value):
 
     import google.generativeai as genai
-    API_KEY = 'AIzaSyBVCi6C7WYXB39p-6xUaZDnjlULaE6LS6E'
+    API_KEY = 'AIzaSyDN8avHb3uwrEhIOtPj7PhkXlpUNeAdsh8'
     genai.configure(api_key=API_KEY)
     model = genai.GenerativeModel('gemini-pro')
     chat = model.start_chat(history=[])
-    r1 = chat.send_message(value)
-    return r1.text
+    q1 = value
+    q2 = 'План диеты'
+    q3 = 'План тренировок'
+    r1 = chat.send_message(q1)
+    r2 = chat.send_message(q2)
+    r3 = chat.send_message(q3)
+    return [r1.text, r2.text, r3.text]
 
 st.header("Рекомендации AI для набора массы")
 
@@ -19,8 +24,10 @@ gender = st.selectbox("Пол", ['Мужчина', 'Женщина'])
 
 btn = st.button("Отправить")
 if btn:
-    query = f"Рекоммендации при весе {weight} кг и при росте {height} см {gender}"
+    query = f"Как можно похудеть или набрать массуы при весе {weight} кг и при росте {height} см {gender}"
 
-    r = load_data(query)
+    r1, r2, r3 = load_data(query)
     if r:
-        st.write(r)
+        st.write(r1)
+        st.write(r2)
+        st.write(r3)
